bccba9f93630d3520ab53010f76c52bf781a88bc
jvrocha91 <107816755+jvrocha91@users.noreply.github.com>
2022-11-07 17:18:23 -0300

    Nome de alunos e novas medidas
    Dados de nome de aluno salvos e adição de medidas para menor de idade e peso

--- .c
+++ .c
@@ -3,6 +3,7 @@
 
 
  struct dev{
+        char nome[10];
         int idade ;
         float peso ;
        };
@@ -10,25 +11,53 @@
 int main() 
 {
  
-  struct dev aluno[3];
+ struct dev aluno[3];
  
  int i;
  for(i=0;i<3;i++){
 
+
+ printf("Nome do aluno :     \n");
+ scanf("%s", &aluno[i].nome);
  printf("Digite a idade do aluno:     \n");
  scanf("%d", &aluno[i].idade);
  printf("Digite o peso:      \n");
  scanf("%f", &aluno[i].peso);
  
  }
- 
+  printf("\n\n\n");
  
  for(i=0;i<3;i++){
- printf("///--------------///\n");
- printf("A idade do aluno : %d\n",aluno[i].idade);
- printf("------------------\n");
- printf("Peso : %f\n",aluno[i].peso);
- printf("------------------\n");
- printf("\n\n\n");
+     
+     
+     if(aluno[i].idade < 18){
+         printf("///--------------///\n");
+         printf("O aluno e menor de idade\n");
+         printf("------------------\n");
+         printf("\n\n\n");
+     }
+     
+     else if  (aluno[i].peso > 90){
+         printf("///--------------///\n");
+         printf("O aluno esta acima do peso\n");
+         printf("------------------\n");
+         printf("\n\n\n");
+      
+     }
+  
+  
+     else{
+         printf("///--------------///\n");   
+         printf("Aluno : %s\n",aluno[i].nome);
+         printf("------------------\n");
+         printf("A idade do aluno : %d\n",aluno[i].idade);
+         printf("------------------\n");
+         printf("Peso : %.2f KG\n",aluno[i].peso);
+         printf("------------------\n");
+         printf("\n\n\n");
+ }}
  
+ return 0;
  }
+
+
